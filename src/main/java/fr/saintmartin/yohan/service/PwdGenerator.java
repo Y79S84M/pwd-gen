ecaa8977bc7e82e978e0e908863f33dfec80fa96
Y79S84M <yostm.dev@gmail.com>
2023-12-20 22:20:44 +0100

    init, basic console script & first Unit Tests

--- src/main/java/fr/saintmartin/yohan/service/PwdGenerator.java
+++ src/main/java/fr/saintmartin/yohan/service/PwdGenerator.java
@@ -2,7 +2,7 @@ package fr.saintmartin.yohan.service;
 
 public class PwdGenerator {
 
-    public PwdGenerator(){};
+    public PwdGenerator(){}
 
     public static String generatePwd(String[] usersInput) {
         return String.join("",usersInput);
